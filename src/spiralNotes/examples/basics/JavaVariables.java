0da0a798557f3d4f666328c50ac518fc1e5871a0
Sanjeev Stephan Murmu <samstephan164@gmail.com>
2025-05-11 14:20:01 +0530

    Added new package - Basics

---
+++ src/spiralNotes/examples/basics/JavaVariables.java
@@ -0,0 +1,12 @@
+package spiralNotes.examples.basics;
+
+public class JavaVariables {
+    public static void main(String[] args)
+    {
+        /*
+        Syntax (to assign a value to variable):
+        data-type <variable-name> = <value>;
+
+         */
+    }
+}
