398829ae21017747ca37715f743253729a346cfa
Sanjeev Stephan Murmu <samstephan164@gmail.com>
2025-05-12 02:05:06 +0530

    adding AccessCLassVariablesMethods class

---
+++ src/spiralNote/codes/AccessClassVariablesMethods.java
@@ -0,0 +1,18 @@
+package spiralNote.codes;
+
+class Car {
+    String brand = "Audi";
+    void start() {
+        System.out.println("Car is Starting");
+    }
+}
+
+
+public class AccessClassVariablesMethods {
+    public static void main(String[] args)
+    {
+        Car car = new Car();
+        System.out.println(car.brand);
+        car.start();
+    }
+}
