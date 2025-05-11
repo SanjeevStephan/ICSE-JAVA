c9a045dc027a88f4c513e466ca8047943daeb15a
Sanjeev Stephan Murmu <samstephan164@gmail.com>
2025-05-12 02:05:22 +0530

    adding DisplayNameOnScreen class

---
+++ src/spiralNote/codes/DisplayNameOnScreen.java
@@ -0,0 +1,15 @@
+package spiralNote.codes;
+
+/**
+ * Real-Life Example Code-#2
+ * File Name : DisplayNameOnScreen.java
+ * Class Name : PrintName
+ * Description : Here's a real-life based example to stores a person name and prints it :
+ */
+
+public class DisplayNameOnScreen {
+    public static void main(String[] args) {
+        String name = "Sanjeev Stephan";
+        System.out.println(name);
+    }
+}
