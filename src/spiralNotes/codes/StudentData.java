91a9961d2f4d33d4e9a09ea22aa59919d3c84211
Sanjeev Stephan Murmu <samstephan164@gmail.com>
2025-05-11 14:31:53 +0530

    added new class StudentData

---
+++ src/spiralNotes/codes/StudentData.java
@@ -0,0 +1,22 @@
+package spiralNotes.codes;
+
+public class StudentData {
+    public static void main(String[] args) {
+        // Student Data
+        String studentName = "Adam Anderson"; // write your name here
+        String studentInClass = "IX";           // write your class name
+        int studentRollno = 17;                // write your roll number
+        int studentAge = 19;                  //  write your age
+        double studentHeightInFt = 5.9;       //  write your height in ft
+        char studentGender = 'M';             // write 'M' for Male & 'F' for Female
+
+        // Print Variables
+        System.out.println("Student Name : " + studentName);
+        System.out.println("Student Class : " + studentInClass);
+        System.out.println("Student Rollno : " + studentRollno);
+        System.out.println("Student Age : " + studentAge);
+        System.out.println("Student Height : " + studentHeightInFt);
+        System.out.println("Student Gender : " + studentGender);
+        
+    }
+}
