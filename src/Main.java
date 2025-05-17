d6399b0707a3dc8198fd32aff376aef51ce3a977
Sanjeev Stephan Murmu <samstephan164@gmail.com>
2025-05-17 08:48:44 +0530

    Made some changes to main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import static spiralNote.codes.EligibleVoter.*;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, Students!");
+        checkAge(12);
+
     }
 }
