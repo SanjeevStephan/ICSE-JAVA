d579d3581322f5e5b1ac0ece542dcf5c6e4bee64
Sanjeev Stephan Murmu <samstephan164@gmail.com>
2025-05-19 09:04:40 +0530

    Added new class JavaMethodOverloading.java to package basics

---
+++ src/spiralNote/examples/basics/JavaMethodOverloading.java
@@ -0,0 +1,28 @@
+package spiralNote.examples.basics;
+
+public class JavaMethodOverloading {
+    static int plusMethod(int x, int y)
+    {
+        return x + y;
+    }
+    static double plusMethod(double x, double y)
+    {
+        return x + y;
+    }
+    static String plusMethod(String a, String b)
+    {
+        String space = "";
+        return a + space + b;
+    }
+
+    public static void main(String[] args)
+    {
+        int myNum1 = plusMethod(4, 8);
+        double myNum2 = plusMethod(12, 18);
+        String myNum3 = plusMethod("9", "8");
+
+        System.out.println("int : " + myNum1);
+        System.out.println("double : " + myNum2);
+        System.out.println("String : " + myNum3);
+    }
+}
